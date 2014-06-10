d66574252f2953c597facb2c090d14417adaf18d
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2014-06-10 10:50:01 +0200

    Reatbed the file

--- jquery.avgrund.js
+++ jquery.avgrund.js
@@ -7,139 +7,139 @@
  */
 
 (function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		// CommonJS
-		module.exports = factory;
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // CommonJS
+        module.exports = factory;
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
 }(function ($) {
-	$.fn.avgrund = function (options) {
-		var defaults = {
-			width: 380, // max = 640
-			height: 280, // max = 350
-			showClose: false,
-			showCloseText: '',
-			closeByEscape: true,
-			closeByDocument: true,
-			holderClass: '',
-			overlayClass: '',
-			enableStackAnimation: false,
-			onBlurContainer: '',
-			openOnEvent: true,
-			setEvent: 'click',
-			onLoad: false,
-			onUnload: false,
-			template: '<p>This is test popin content!</p>',
+    $.fn.avgrund = function (options) {
+        var defaults = {
+            width: 380, // max = 640
+            height: 280, // max = 350
+            showClose: false,
+            showCloseText: '',
+            closeByEscape: true,
+            closeByDocument: true,
+            holderClass: '',
+            overlayClass: '',
+            enableStackAnimation: false,
+            onBlurContainer: '',
+            openOnEvent: true,
+            setEvent: 'click',
+            onLoad: false,
+            onUnload: false,
+            template: '<p>This is test popin content!</p>',
             afterComplete: false
-		};
+        };
 
-		options = $.extend(defaults, options);
+        options = $.extend(defaults, options);
 
-		return this.each(function() {
-			var self = $(this),
-				body = $('body'),
-				maxWidth = options.width > 640 ? 640 : options.width,
-				maxHeight = options.height > 350 ? 350 : options.height,
-				template = typeof options.template === 'function' ? options.template(self) : options.template;
+        return this.each(function() {
+            var self = $(this),
+                body = $('body'),
+                maxWidth = options.width > 640 ? 640 : options.width,
+                maxHeight = options.height > 350 ? 350 : options.height,
+                template = typeof options.template === 'function' ? options.template(self) : options.template;
 
-			body.addClass('avgrund-ready');
+            body.addClass('avgrund-ready');
 
-			if ($('.avgrund-overlay').length === 0) {
-				body.append('<div class="avgrund-overlay ' + options.overlayClass + '"></div>');
-			}
+            if ($('.avgrund-overlay').length === 0) {
+                body.append('<div class="avgrund-overlay ' + options.overlayClass + '"></div>');
+            }
 
-			if (options.onBlurContainer !== '') {
-				$(options.onBlurContainer).addClass('avgrund-blur');
-			}
+            if (options.onBlurContainer !== '') {
+                $(options.onBlurContainer).addClass('avgrund-blur');
+            }
 
-			function onDocumentKeyup (e) {
-				if (options.closeByEscape) {
-					if (e.keyCode === 27) {
-						deactivate();
-					}
-				}
-			}
+            function onDocumentKeyup (e) {
+                if (options.closeByEscape) {
+                    if (e.keyCode === 27) {
+                        deactivate();
+                    }
+                }
+            }
 
-			function onDocumentClick (e) {
-				if (options.closeByDocument) {
-					if ($(e.target).is('.avgrund-overlay, .avgrund-close')) {
-						e.preventDefault();
-						deactivate();
-					}
-				} else if ($(e.target).is('.avgrund-close')) {
-						e.preventDefault();
-						deactivate();
-				}
-			}
+            function onDocumentClick (e) {
+                if (options.closeByDocument) {
+                    if ($(e.target).is('.avgrund-overlay, .avgrund-close')) {
+                        e.preventDefault();
+                        deactivate();
+                    }
+                } else if ($(e.target).is('.avgrund-close')) {
+                        e.preventDefault();
+                        deactivate();
+                }
+            }
 
-			function activate () {
-				if (typeof options.onLoad === 'function') {
-					options.onLoad(self);
-				}
+            function activate () {
+                if (typeof options.onLoad === 'function') {
+                    options.onLoad(self);
+                }
 
-				setTimeout(function() {
-					body.addClass('avgrund-active');
-				}, 100);
+                setTimeout(function() {
+                    body.addClass('avgrund-active');
+                }, 100);
 
-				var $popin = $('<div class="avgrund-popin ' + options.holderClass + '"></div>');
-				$popin.append(template);
-				body.append($popin);
+                var $popin = $('<div class="avgrund-popin ' + options.holderClass + '"></div>');
+                $popin.append(template);
+                body.append($popin);
 
-				$('.avgrund-popin').css({
-					'width': maxWidth + 'px',
-					'height': maxHeight + 'px',
-					'margin-left': '-' + (maxWidth / 2 + 10) + 'px',
-					'margin-top': '-' + (maxHeight / 2 + 10) + 'px'
-				});
+                $('.avgrund-popin').css({
+                    'width': maxWidth + 'px',
+                    'height': maxHeight + 'px',
+                    'margin-left': '-' + (maxWidth / 2 + 10) + 'px',
+                    'margin-top': '-' + (maxHeight / 2 + 10) + 'px'
+                });
 
-				if (options.showClose) {
-					$('.avgrund-popin').append('<a href="#" class="avgrund-close">' + options.showCloseText + '</a>');
-				}
+                if (options.showClose) {
+                    $('.avgrund-popin').append('<a href="#" class="avgrund-close">' + options.showCloseText + '</a>');
+                }
 
-				if (options.enableStackAnimation) {
-					$('.avgrund-popin').addClass('stack');
-				}
+                if (options.enableStackAnimation) {
+                    $('.avgrund-popin').addClass('stack');
+                }
 
-				body.bind('keyup', onDocumentKeyup)
-					.bind('click', onDocumentClick);
+                body.bind('keyup', onDocumentKeyup)
+                    .bind('click', onDocumentClick);
 
-				if (typeof options.afterComplete === 'function') {
-					options.afterComplete(self);
-				}
-			}
+                if (typeof options.afterComplete === 'function') {
+                    options.afterComplete(self);
+                }
+            }
 
-			function deactivate () {
-				body.unbind('keyup', onDocumentKeyup)
-					.unbind('click', onDocumentClick)
-					.removeClass('avgrund-active');
+            function deactivate () {
+                body.unbind('keyup', onDocumentKeyup)
+                    .unbind('click', onDocumentClick)
+                    .removeClass('avgrund-active');
 
-				setTimeout(function() {
-					$('.avgrund-popin').remove();
-				}, 500);
+                setTimeout(function() {
+                    $('.avgrund-popin').remove();
+                }, 500);
 
-				if (typeof options.onUnload === 'function') {
-					options.onUnload(self);
-				}
-			}
+                if (typeof options.onUnload === 'function') {
+                    options.onUnload(self);
+                }
+            }
 
-			if (options.openOnEvent) {
-				self.bind(options.setEvent, function (e) {
-					e.stopPropagation();
+            if (options.openOnEvent) {
+                self.bind(options.setEvent, function (e) {
+                    e.stopPropagation();
 
-					if ($(e.target).is('a')) {
-						e.preventDefault();
-					}
+                    if ($(e.target).is('a')) {
+                        e.preventDefault();
+                    }
 
-					activate();
-				});
-			} else {
-				activate();
-			}
-		});
-	};
+                    activate();
+                });
+            } else {
+                activate();
+            }
+        });
+    };
 }));
